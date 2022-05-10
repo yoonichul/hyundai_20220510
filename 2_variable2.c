dc5d8e286482425dfd436d8cdc394c41d26e5221
hcyoon <yhc2050@gmail.com>
2022-05-10 11:55:41 +0900

    20220510 UPDATE 1. Add content on 2_variable2.c 2. Add file that is 3_constant.c

--- 2_variable2.c
+++ 2_variable2.c
@@ -9,6 +9,21 @@
       - 지금은 어디서든 지역 변수를 생성할 수 있습니다.
     5. double을 int에 대입할 경우, 
        소수점이 사라집니다.
+    6. 변수의 주소와 크기
+      1) 변수는 메모리에 할당됩니다.
+      2) 변수의 선언에서 이름을 뺀 나머지가 타입입니다.
+         Declaration         Type
+         int n;           => int
+         int arr[10]      => int[10]
+         void foo(int a); => void(int a)
+      3) 변수의 메모리 크기를 얻기 위해서는 sizeof 연산자를 사용하면 됩니다.
+        => unsigned long   
+           sizeof(타입)
+           sizeof(변수) / sizeof 변수
+      4) 모든 변수는 메모리에 할당되고, 메모리의 주소를 가집니다.
+        => &: 주소연산자
+        => 주소를 출력하는 서식은 %p 입니다.
+        => 주소의 크기는 32bit는 4 byte / 64 bit는 8바이트 입니다.
 */
 
 int main(void)
@@ -25,5 +40,14 @@ int main(void)
     
     printf("b = %d\n",b);
     
+    printf("%lu\n",sizeof(char));
+    printf("%lu\n",sizeof(unsigned int));
+    printf("%lu\n",sizeof(unsigned long long));
+    printf("%lu\n",sizeof a);
+    printf("%p\n",&a);
+    printf("%lu\n",sizeof(&a));
+
+
+
     return 0;
 }
