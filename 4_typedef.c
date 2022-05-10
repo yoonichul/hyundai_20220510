616a1839d2a726f75180c7bec4a90f441c4c9295
hcyoon <yhc2050@naver.com>
2022-05-10 13:43:57 +0900

    20220510 UPDATE

---
+++ 4_typedef.c
@@ -0,0 +1,24 @@
+// 4_typedef.c
+#include <stdio.h>
+
+/*
+    1. typedef는 기존 타입에 새로운 별명을 부여할 수 있습니다.
+    2. typedef 활용
+      > 복잡한 타입 표현식을 typedef를 통해 간결하게 표현할 수 있습니다.
+*/
+typedef int int32;
+typedef char int8;
+
+foo()
+{
+    int x[3];
+    return x;
+}
+
+int main(void)
+{
+    int32 a;
+    int8 b;
+
+    return 0;
+}
