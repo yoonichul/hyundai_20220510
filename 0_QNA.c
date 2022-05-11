9052ee6a7b699856811300f8447d7d1a1d3a1732
hcyoon <yhc2050@naver.com>
2022-05-11 10:07:55 +0900

    220511 UPDATE

---
+++ 0_QNA.c
@@ -0,0 +1,145 @@
+/*
+    Array / Array Pointer
+    - Array : 연속된 메모리를 할당할 수 있습니다.
+    - Pointer : 변수의 주소를 가지고 있는 타입.
+*/
+#include <stdio.h>
+// Pointer
+#if 0 
+int main(void)
+{
+    int n = 10;
+    printf("%p\n",&n);
+    
+    int *p= &n;
+    printf("%p\n",p);
+
+    double d = 3.14;
+    double* pd = &d;
+    printf("%lf\n",*pd);
+
+    return 0;
+}
+#endif
+/*
+    Array
+        - 배열의 이름은 배열의 첫번째 원소의 시작주소로 해석됩니다.
+          => Decay
+          => 예외
+            1)sizeof
+            2)&
+        - 길이 정보를 추가적으로 받아야 합니다.
+*/
+#if 0 
+void print(int *x, int n)
+{
+    for(int i = 0; i<n; i++)
+    {
+        printf("%d ",x[i]);
+    }
+    printf("\n");
+}
+
+int main(void)
+{
+    int x[3] = {1, 2, 3};
+    // print(x, sizeof(x)/sizeof(int));
+    print(x, sizeof(x)/sizeof(x[0]));
+    return 0;
+}
+#endif
+
+#if 0 
+void print(int (*x)[3], int n)
+{
+    for(int i = 0; i<n; i++)
+    {
+        for(int j = 0; j<3; j++)
+        {
+            printf("%d ",x[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main(void)
+{
+    int x[2][3] = {{1,2,3},{2,3,4}};
+    print(x, sizeof(x)/sizeof(x[0]));
+    // print(&x[0]) 
+    // int(*p)[3];
+
+    return 0;
+}
+#endif
+
+#if 0
+void foo(int *x)
+{
+    printf("foo : %lu\n",sizeof(x)); // 8 byte
+}
+
+int main()
+{
+    int x[3];
+    int *p = x;
+    int (*p2)[3]=&x;
+
+    printf("main : %lu\n",sizeof(x)); //12 byte
+    foo(x);
+}
+#endif
+
+#if 0
+typedef int (*PARR3)[3];
+PARR3 foo(void)
+// int (*foo())[3]
+{
+    static int x[3] = {10, 20, 30};
+    printf("%d %d %d\n", x[0], x[1], x[2]);
+    return &x;
+}
+
+int main() {
+    int x[3];
+    int *p = x;
+    int(*p2)[3] = &x;
+
+    printf("main : %lu\n", sizeof(x)); // 12 byte
+    foo();
+}
+#endif
+
+/*
+    int (*foo())[3]
+    1. foo()
+     -> foo는 함수입니다.
+    2. (*foo())
+     -> 반환 타입은 포인터입니다.
+    3.  int (*foo())[3]
+     -> 포인터가 가르키는 타입은 int[3] 배열입니다.
+*/
+
+/*
+    * C / C++
+    a.c  -> a.o -> a.out / a.exe
+        - LLVM(Compiler)
+            C/C++ -> IL(Intermediate Language) -> a.o -> a.out
+    * JAVA
+    A.java -> A.class(ByteCode, IL) <- VM(java.exe / java)
+         Compile
+        - Language
+            Jython
+            JRuby
+            Kotlin
+            Scala
+    MONO Project
+
+
+    * C# / A.cs     -> A.obj(IL)    <- CLR
+      C++
+      VB
+      F#
+
+    Wirte Once, Run Anywhere
+*/
