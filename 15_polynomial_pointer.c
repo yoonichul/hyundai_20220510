bd4cb61df718a5205d994e7180153c449f317db7
hcyoon <yhc2050@naver.com>
2022-05-12 10:50:11 +0900

    20220512 REVIEW

--- 15_polynomial_pointer.c
+++ 15_polynomial_pointer.c
@@ -5,6 +5,7 @@
     ppn ----> pn ----> n  [100]
 
 */
+#if 0
 int main(void)
 {
     int n = 100;
@@ -16,3 +17,27 @@ int main(void)
 
     return 0;
 }
+#endif
+
+#if 1
+void pswap(int **ppa, int **ppb) {
+    int *temp = *ppa;
+    *ppa = *ppb;
+    *ppb = temp;
+}
+
+int main(void) {
+    const int a = 10;
+    const int b = 20;
+    int *pa = &a;
+    int *pb = &b;
+    printf("pa: %d pb: %d\n", *pa, *pb);
+    pswap(&pa, &pb);
+    // pa = pb;
+    // pb = *temp;
+    printf("a: %d b: %d\n", a, b);
+    printf("pa: %d pb: %d\n", *pa, *pb);
+
+    return 0;
+}
+#endif
