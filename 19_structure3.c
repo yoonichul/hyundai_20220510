c52e5b30e6e60be1972c358063d42cad7560ed9d
hcyoon <yhc2050@naver.com>
2022-05-13 14:32:15 +0900

    20220513 UPDATE

--- 19_structure3.c
+++ 19_structure3.c
@@ -1,19 +1,34 @@
 // 19_structure.c
 #include <stdio.h>
-
-typedef struct _point{
+#include <stdlib.h>
+// 구조체 태그명 : _point
+// 구조체 타입 : struct point
+typedef struct _point {
+    // 구조체 멤버 변수
     int x;
     int y;
-}POINT;
+} POINT;
 
+int main(void) {
+    // POINT pt1;
+    POINT *pt2 = (POINT *)malloc(sizeof(POINT));
 
+    // POINT pt1 = {10, 20};
+    // C-99 - 멤버 지정 초기화
+    // POINT pt1 = {.x = 10, .y = 20};
+    POINT pt1 = {
+        0,
+        0,
+    };
 
-int main(void)
-{
-    POINT pt1;
+    pt2->x = 100;
+    pt2->y = 200;
+    // .(dot) 연산자 : 구조체 멤버 변수에 접근이 가능합니다.
+    // pt1.x = 10;
+    // pt1.y = 20;
 
-    pt1.x = 10;
-    pt1.y = 20;
-    
+    printf("%d %d\n", pt1.x, pt1.y);
+    printf("%d %d\n", pt2->x, pt2->y);
+
     return 0;
 }
