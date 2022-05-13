54fd2d436f7c723f5376b5c893c86c401f755107
hcyoon <yhc2050@naver.com>
2022-05-13 10:26:39 +0900

    20220513 UPDATE

---
+++ 0_QnA5.c
@@ -0,0 +1,12 @@
+// 0_QnA5.c
+#include <stdio.h>
+int main(void) {
+#if 0
+    printf("Hello\n");
+    printf("Hello\n");
+    printf("Hello\n");
+    printf("Hello\n");
+    printf("Hello\n");
+#endif
+    return 0;
+}
