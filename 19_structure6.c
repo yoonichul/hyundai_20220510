0280c5172dafd42f87caf88317663965af7e5ecd
hcyoon <yhc2050@naver.com>
2022-05-13 15:56:06 +0900

    20220513 UPDATE

--- 19_structure6.c
+++ 19_structure6.c
@@ -1,6 +1,6 @@
 // 19_structure6.c
 #include <stdio.h>
-
+#include <stdlib.h>
 typedef struct point {
     int x;
     int y;
@@ -29,6 +29,30 @@ POINT add2(const POINT *pa, const POINT *pb) {
     result.y = pa->y + pb->y;
     return result;
 }
+// 결과도 포인터 파라미터를 통해 전달해야 합니다.
+//  => Out Parameter
+void add3(const POINT *pa, const POINT *pb, POINT *result) {
+    result->x = pa->x + pb->x;
+    result->y = pa->y + pb->y;
+}
+// 1) malloc
+// 2) static
+//    => 함수가 정적 메모리(정적 지역 변수 / 전역 변수)에 의존하는 경우,
+//       "재진입이 불가능하다"라고 합니다.
+POINT *add4(const POINT *pa, const POINT *pb) {
+    POINT *result = malloc(sizeof(POINT));
+    result->x = pa->x + pb->x;
+    result->y = pa->y + pb->y;
+    return result;
+}
+
+POINT *add5(const POINT *pa, const POINT *pb) {
+    static POINT result;
+    result.x = pa->x + pb->x;
+    result.y = pa->y + pb->y;
+    return &result;
+}
+
 
 int main(void) {
     printf("%lu\n", sizeof(POINT));
@@ -36,7 +60,15 @@ int main(void) {
     POINT pt2 = {.x = 100, .y = 200};
 
     POINT result = add1(pt1, pt2);
+    result = add2(&pt1, &pt2);
+    // ----------------------------------------
+    add3(&pt1, &pt2, &result);
     printf("%d %d\n", result.x, result.y);
-
+    POINT *result2 = add4(&pt1, &pt2);
+    printf("%d %d\n", result2->x, result2->y);
+    free(result2);
+    result2 =add5(&pt1,&pt2);
+    printf("%d %d\n", result2->x, result2->y);
+    
     return 0;
 }
