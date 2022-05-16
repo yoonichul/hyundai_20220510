519faf3b7e4a633e03cc8add02081b08cd2b7cc8
hcyoon <yhc2050@naver.com>
2022-05-16 09:55:02 +0900

    20220516 REVIEW

--- 19_structure2.c
+++ 19_structure2.c
@@ -11,16 +11,18 @@ struct point{
 typedef struct point POINT;
 #endif
 
-#if 0 
+#if 1 
 typedef struct point {
     int x;
     int y;
 }POINT;
 #endif
+#if 0
 typedef struct {
     int x;
     int y;
 } POINT;
+#endif
 struct {
     int x;
     int y;
@@ -31,14 +33,16 @@ struct
     int y;
 }XXX3;
 
+#if 0
 int main(void) {
     POINT pt;
     // XXX1 = XXX3; // 내용은 같지만 다른 타입의 구조체입니다.
     XXX1 = XXX2; // 태그명에 XXX1, XXX2 같이 선언되어있으므로 두 개의 구조체는 같은 타입입니다.
     return 0;
 }
+#endif
 
-#if 0
+#if 1
 // 전역 변수
 struct point pt1;
 int main(void) {
